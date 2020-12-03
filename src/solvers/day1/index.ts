f1a8be8f9a6e3229fc5cdee23d96ec1d83b6b2c0
Scott Dose <sdose27@gmail.com>
2020-12-02 19:46:48 -0800

    New soluton to day 1 (after viewing solutions)

--- src/solvers/day1/index.ts
+++ src/solvers/day1/index.ts
@@ -1,36 +1,47 @@
 import * as _ from 'lodash';
+import * as fp from 'lodash/fp';
+import { combinations } from '../../utils';
 
-const target = 2020;
+
+/**
+ * Returns a solution to the n-sum problem (i.e. find 2 numbers that sum up to 2020).
+ * 
+ * @param target - the target sum
+ * @param n - the number of items which are required to sum to target
+ */
+const nSum = _.curry((target: number, n: number) => {
+  interface ReduceResult {
+    answer: Array<number>;
+    dict: Map<number, number>;
+  }
+  const combos = fp.curry((n: number, arr: Array<number>) => combinations(n, arr))(n - 1);
+  const reducer = (acc: ReduceResult, values: Array<number>): ReduceResult => {
+    values.forEach(value => acc.dict.set(value, (acc.dict.get(value) || 0) + 1));
+    const inverse = target - _.sum(values);
+    if (acc.answer.length === 0 && acc.dict.get(inverse)) {
+      acc.answer = values.concat(inverse);
+    }
+    return acc;
+  };
+  return fp.compose(
+    fp.prop('answer'),
+    fp.reduce(reducer, { answer: [], dict: new Map<number, number>() }),
+    combos,
+    fp.map(parseInt),
+    fp.split('\n'),
+  );
+});
 
 export const solvePart1 = (input: string): void => {
-  const nums = _.map(_.split(input, '\n'), x => parseInt(x, 10));
-  const numDict = _.keyBy(nums);
-
-  const ans1 = nums.find((x: number) => numDict[target-x]);
-  if (!ans1) {
-    throw new Error(`could not find pair summing to ${target}`);
-  }
-  const ans2 = target - ans1;
-  console.log('part 1 answer:', ans1 * ans2);
+  const solver = nSum(2020, 2); 
+  const answers = solver(input) as Array<number>;
+  const answer = fp.reduce(fp.multiply, 1)(answers); 
+  console.log(`part 1 answer is ${answer}`);
 };
 
 export const solvePart2 = (input: string): void => {
-  const nums = _.map(_.split(input, '\n'), x => parseInt(x, 10));
-  const numDict = _.keyBy(nums);
-
-  const pairs = _.flatten(
-    _.map(nums, (x: number, i: number) => 
-      _.map(
-        _.filter(nums, (_y: number, j: number) => j !== i ),
-        (y: number) => [x, y]
-      )
-  )
-  );
-
-  const answers = pairs.find((pairs: Array<number>) => numDict[target - pairs[0] - pairs[1]]);
-  if (!answers) {
-    throw new Error(`could not find triplet that sums to ${target}`);
-  }
-  const [answer1, answer2] = answers;
-  console.log('part 2 answer:', answer1 * answer2 * (target - answer1 - answer2));
+  const solver = nSum(2020, 3);
+  const answers = solver(input) as Array<number>;
+  const answer = fp.reduce(fp.multiply, 1)(answers); 
+  console.log(`part 2 answer is ${answer}`);
 };
---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+import * as _ from 'lodash';
+
+export const combinations = <T>(n: number, arr: Array<T>): Array<Array<T>> => {
+  const combinationsRecurse = (n: number, arr: Array<T>): Array<Array<T>> => {
+    if (n === 0) return [[]];
+    else if (n === 1) return _.chunk(arr, 1);
+
+    return _.flatten(
+      _.map(arr,(ele, idx) => {
+        const prevcombinations = combinationsRecurse(n-1, _.slice(arr, idx + 1, arr.length));
+        const combined = _.map(prevcombinations, (el) => el.concat([ele]));
+        return combined; 
+      })
+    );
+  };
+
+  return combinationsRecurse(n, arr);
+};
